9e1bf41b09ee66182fd7f7d87ffa2ae5783d3e01
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-22 15:23:00 -0300

    Gnerating a secret_number.

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,15 @@
 use std::io;
+use rand::Rng;
 
 fn main() {
   println!("Guess the number!");
+
+  // The kind of range expression we’re using here takes the form start..end.
+  // It is inclusive on the lower bound but exclusive on the upper bound.
+  // Alternatively, we could pass the range 1..=100.
+  let secret_number = rand::thread_rng().gen_range(1..101);
+
+  println!("The secret number is: {}.", secret_number);
 
   println!("Please input your guess.");
 

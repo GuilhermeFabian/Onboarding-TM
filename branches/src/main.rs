19843f573d32ecf646e4e884e3147446c66f23cd
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-22 17:51:08 -0300

    If, else if and else

--- branches/src/main.rs
+++ branches/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+  const CONDITION: bool = true;
+  const NUMBER: i32 = if CONDITION { 5 } else { 6 };
+
+  if NUMBER % 4 == 0 {
+    println!("Number is divisible by 4.");
+  } else if NUMBER % 3 == 0 {
+    println!("Number is divisible by 3.");
+  } else if NUMBER % 2 == 0 {
+    println!("Number is divisible by 2.");
+  } else {
+    println!("Number is not divisible by 4, 3, or 2.");
+  }
 }

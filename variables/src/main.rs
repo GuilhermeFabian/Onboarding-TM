c58c128d2a8597e5a7bfcaf4ed2067d0182b449e
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-22 16:16:37 -0300

    Finishes variables chapter.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+  let x = 5;
+
+  // We’re effectively creating a new variable when we use the let keyword again,
+  // we can change the type of the value but reuse the same name.
+  let x = x + 1;
+
+  {
+      let x = x * 2;
+      println!("The value of x in the inner scope is: {}", x);
+  }
+
+  println!("The value of x is: {}", x);
 }

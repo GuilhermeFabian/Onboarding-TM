ae22700b1f474e85433424d77cc2ede6628b5530
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-22 17:33:43 -0300

    Adds an example of function with return

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -1,9 +1,15 @@
 fn main() {
   println!("Hello, world!");
 
-  another_function(5, 'h');
+  let five = five();
+
+  another_function(five, 'h');
 }
 
 fn another_function(value: i32, unit_label: char) {
   println!("The measurement is: {}{}.", value, unit_label);
+}
+
+fn five() -> i32 {
+  5
 }

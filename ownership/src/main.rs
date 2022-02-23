d0992b1e62736bce8f72699b562f9ae854db725b
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-23 13:08:51 -0300

    Primitive types copy example.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -2,5 +2,11 @@ fn main() {
   let s1 = String::from("Hello!");
   let s2 = s1.clone();
 
-  println!("{}", s1);
+  println!("s1 = {}, s2 = {}", s1, s2);
+
+  let x = 5;
+  // Primitive types can be copied without invalidating the variables from which it was copied.
+  let y = x;
+
+  println!("x = {}, y = {}", x, y);
 }

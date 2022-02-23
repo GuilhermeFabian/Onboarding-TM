c2b861ad5ebf59ab61403ba07aabf945d00f133a
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-23 14:20:24 -0300

    Reference example.

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,3 +1,13 @@
 fn main() {
-  println!("Hello, world!");
+  let s1 = String::from("Hello!");
+  let len = calculate_length(&s1);
+
+  println!("The length of '{}' is {}", s1, len);
+}
+
+// A reference is like a pointer in that it’s an address
+// we can follow to access data stored at that address that is owned by some other variable.
+// Unlike a pointer, a reference is guaranteed to point to a valid value of a particular type.
+fn calculate_length(s: &String) -> usize {
+  s.len()
 }

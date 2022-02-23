a3af8ec801fc87e5a9cf24ae323982a19ceb136b
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-23 13:19:02 -0300

    Ownership and functions.

--- ownership/src/main.rs
+++ ownership/src/main.rs
@@ -9,4 +9,20 @@ fn main() {
   let y = x;
 
   println!("x = {}, y = {}", x, y);
+
+  takes_ownership(s2); // s2's value moves into the function...
+                                  // ... and so is no longer valid here
+
+  makes_copy(y);      // x would move into the function,
+                                  // but i32 is Copy, so it's okay to still
+                                  // use x afterward
+}
+
+fn takes_ownership(some_string: String) {
+  println!("{}", some_string);
+} // Here, some_string goes out of scope and `drop` is called. The backing
+  // memory is freed.
+
+fn makes_copy(some_integer: i32) {
+  println!("{}", some_integer);
 }

979cfe553e3e6d75397839f720bf59a720785c71
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-23 14:35:10 -0300

    Mutable reference example.

--- references_and_borrowing/src/main.rs
+++ references_and_borrowing/src/main.rs
@@ -1,7 +1,11 @@
 fn main() {
-  let s1 = String::from("Hello!");
+  let mut s1 = String::from("Hello");
   let len = calculate_length(&s1);
 
+  println!("The length of '{}' is {}", s1, len);
+
+  change(&mut s1);
+  let len = calculate_length(&s1);
   println!("The length of '{}' is {}", s1, len);
 }
 
@@ -10,4 +14,8 @@ fn main() {
 // Unlike a pointer, a reference is guaranteed to point to a valid value of a particular type.
 fn calculate_length(s: &String) -> usize {
   s.len()
+}
+
+fn change(s: &mut String) {
+  s.push_str(", World!");
 }

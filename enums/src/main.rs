b0cf6a73ece8d7bd1b5f1d4bc0632e5165086ff8
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-24 11:34:27 -0300

    Enum with methods.

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,3 +1,18 @@
+#[derive(Debug)]
+enum Message {
+  Quit,
+  Move { x: i32, y: i32 },
+  Write(String),
+  ChangeColor(u8, u8, u8),
+}
+
+impl Message {
+  fn call(&self) {
+    dbg!(self);
+  }
+}
+
 fn main() {
-  println!("Hello, world!");
+  let m = Message::ChangeColor(255, 255, 255);
+  m.call();
 }

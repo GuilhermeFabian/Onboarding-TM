01f869bf915760b677deaca332c70dbd48bf5c74
Fabian <guilhermeferreirafabian@gmail.com>
2022-02-24 10:38:06 -0300

    Struct methods

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -4,12 +4,45 @@ struct Rectangle {
   height: u32,
 }
 
+// Instance Methods
+impl Rectangle {
+  fn area(&self) -> u32 {
+    self.width * self.height
+  }
+
+  fn can_hold(&self, other: &Rectangle) -> bool {
+    self.width > other.width && self.height > other.height
+  }
+}
+
+// Static methods
+impl Rectangle {
+  fn square(size: u32) -> Rectangle {
+    Rectangle {
+      width: size,
+      height: size,
+    }
+  }
+}
+
 fn main() {
   let scale = 2;
   let rect1 = Rectangle {
     width: dbg!(30 * scale),
     height: 50,
   };
+
+  let rect2 = Rectangle {
+    width: 20,
+    height: 30,
+  };
+
+  let square = Rectangle::square(10);
+
+  println!("The area of the rectangle is {} square pixels.", rect1.area());
+  println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+  println!("Can rect2 hold rect1? {}", rect2.can_hold(&rect1));
+  println!("Can rect2 hold square? {}", rect2.can_hold(&square));
 
   dbg!(rect1);
 }
